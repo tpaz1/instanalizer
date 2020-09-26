844b9519acab864b1180954414e58a018f44a4dc
tpaz1 <tompazz16@gmail.com>
2020-09-27 01:57:32 +0300

    commit of the first python version of instalayzer

--- instanalyzer.py
+++ instanalyzer.py
@@ -1,18 +1,26 @@
+__author__ = "Tom Paz"
+__email__ = "tompazz16@gmail.com"
+__date__ = "27/09/2020"
+
 from InstagramAPI import InstagramAPI
 from time import sleep
 
 class instanalyzer:
+""" This class defines an instagram program that uses instagram api to unfollow users that dont follow you back """ 
 
     def __init__(self):
+    """ This function defines class variables """    
         self.following_users = []
         self.follower_users = []      
         self.api = ""
 
     def login(self, username, password):
+    """ This function login to instagram account """
         self.api = InstagramAPI(username, password)
         self.api.login()
 
     def unfollow_users(self):
+    """ This function stores the account followers and following in a list, then unfollow users that dont follow back """
         api = self.api
         api.getSelfUserFollowers()
         result = api.LastJson
